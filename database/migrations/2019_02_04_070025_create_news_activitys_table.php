3e4882307a9801dad4920ea15c5f60e42c73b562
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-02-04 15:23:53 +0700

    create migration news and activities

---
+++ database/migrations/2019_02_04_070025_create_news_activitys_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsActivitysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news_activitys', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('cate_id')->nullable();
+            $table->string('title');
+            $table->string('description');
+            $table->text('detail');
+            $table->string('thumb');
+            $table->string('image')->nullable();
+            $table->string('keywords')->nullable();
+            $table->string('file')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news_activitys');
+    }
+}
