cff7f8e7ae01ec9edcedd2c48a2c9a07df8f3f97
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-02-04 15:21:44 +0700

    create migration contact form

---
+++ database/migrations/2019_02_04_081000_create_contact_forms_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactFormsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_forms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('title')->nullable();
+            $table->string('topic')->nullable();
+            $table->text('detail')->nullable();
+            $table->string('telephone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('file')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact_forms');
+    }
+}
