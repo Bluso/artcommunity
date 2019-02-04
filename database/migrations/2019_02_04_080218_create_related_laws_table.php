d6d8ea567d41f7f075b40456ccf202d71d201aff
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-02-04 15:22:29 +0700

    create migration related laws

---
+++ database/migrations/2019_02_04_080218_create_related_laws_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRelatedLawsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('related_laws', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('law_cate_id')->nullable();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->text('detail')->nullable();
+            $table->string('thumb')->nullable();
+            $table->string('image')->nullable();
+            $table->string('keywords')->nullable();
+            $table->string('file')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('related_laws');
+    }
+}
