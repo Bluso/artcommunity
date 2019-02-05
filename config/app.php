7b092434477a55b698a8d02bfdfc205e383073e4
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-02-05 19:51:11 +0700

    use image libraries

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         JeroenNoten\LaravelAdminLte\ServiceProvider::class,
+        'Intervention\Image\ImageServiceProvider',
 
     ],
 
@@ -227,7 +228,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-
+        'Image' => 'Intervention\Image\Facades\Image',
     ],
 
 ];
