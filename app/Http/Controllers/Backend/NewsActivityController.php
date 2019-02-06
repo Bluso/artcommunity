22fad2673f1332edec2170886cb56de77251792e
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-02-06 14:43:28 +0700

    create news controller

---
+++ app/Http/Controllers/Backend/NewsActivityController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\NewsActivitys;
+use Illuminate\Http\Request;
+
+class NewsActivityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $news = NewsActivitys::all();
+        return view('backend.news.index')->withNews($news);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\NewsActivitys  $newsActivitys
+     * @return \Illuminate\Http\Response
+     */
+    public function show(NewsActivitys $newsActivitys)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\NewsActivitys  $newsActivitys
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(NewsActivitys $newsActivitys)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\NewsActivitys  $newsActivitys
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, NewsActivitys $newsActivitys)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\NewsActivitys  $newsActivitys
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(NewsActivitys $newsActivitys)
+    {
+        //
+    }
+}
