ec19b82a7883c2890129bf6f032ac3aaabd35cf7
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-02-11 12:45:18 +0700

    route laws

--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,24 @@ Auth::routes();
             });
             
         });
+        Route::prefix('laws')->group(function () {
+            Route::post('upload/image', 'Backend\NewsActivityController@upload_image');
+            Route::get('/', 'Backend\LawsController@index');
+            Route::get('add', 'Backend\LawsController@create');
+            Route::post('add', 'Backend\LawsController@store');
+            Route::get('edit/{id}', 'Backend\LawsController@edit');
+            Route::post('edit/{id}', 'Backend\LawsController@update');
+            Route::get('delete/{id}', 'Backend\LawsController@destroy');
+            Route::prefix('cate')->group(function () {
+                Route::post('upload/image', 'Backend\CateLawsController@upload_image');
+                Route::get('/', 'Backend\CateLawsController@index');
+                Route::get('add', 'Backend\CateLawsController@create');
+                Route::post('add', 'Backend\CateLawsController@store');
+                Route::get('edit/{id}', 'Backend\CateLawsController@edit');
+                Route::post('edit/{id}', 'Backend\CateLawsController@update');
+                Route::get('delete/{id}', 'Backend\CateLawsController@destroy');
+            });
+        });
         Route::prefix('contact')->group(function () {
             Route::get('/', 'Backend\ContactDataController@index');
             Route::post('save', 'Backend\ContactDataController@store'); 
