0aec1c69e3ad3b5d3e463eb5a13f1d9d0ed3e084
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-02-12 12:42:16 +0700

    create file input lib

---
+++ resources/views/backend/layouts/css_fileinput.blade.php
@@ -0,0 +1,5 @@
+<link  rel="stylesheet" src="{{ asset('vendor/bootstrap-fileinput/fileinput.min.css') }}">
+<style>
+.file-caption-name{border:none;width: 90%;}
+label{width:100%}
+</style>
---
+++ resources/views/backend/layouts/js_fileinput.blade.php
@@ -0,0 +1,12 @@
+<script src="{{ asset('vendor/bootstrap-fileinput/js/plugins/piexif.min.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap-fileinput/js/plugins/sortable.min.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap-fileinput/js/plugins/purify.min.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap-fileinput/js/plugins/purify.min.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap-fileinput/js/fileinput.min.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap-fileinput/themes/fa/theme.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap-fileinput/js/locales/th.js') }}"></script>
+<script>
+$("#image").fileinput({'showUpload':false, 'previewFileType':'any'});
+$("#thumb").fileinput({'showUpload':false, 'previewFileType':'any'});
+$("#file").fileinput({'showUpload':false, 'previewFileType':'any'});
+</script>
