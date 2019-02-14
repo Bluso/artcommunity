704b18c81b8b88c69d34faf7df2c30dfbcb48626
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-02-14 18:04:05 +0700

    home query data from model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use App\KnowledgeResearch;
+use App\NewsActivitys;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.index');
+        $knowledge = KnowledgeResearch::latest()->limit(4)->get();
+        $news = NewsActivitys::latest()->get();
+        return view('home.index')
+        ->withKnowledge($knowledge)
+        ->withNews($news);
     }
 }
