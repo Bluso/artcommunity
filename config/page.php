841a7f689a9a548fcad236227b84d5696ccfb34a
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-02-20 18:35:44 +0700

    create config file page

---
+++ config/page.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'page_list' => [
+        '1' => 'Home',
+        '2' => 'About',
+        '3' => 'News & Activity',
+        '4' => 'Laws',
+        '5' => 'Knowledge',
+        '6' => 'Contact',
+    ],
+    
+];
