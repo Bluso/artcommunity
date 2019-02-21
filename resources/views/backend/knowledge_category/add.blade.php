d871a6247d5c94715218bd72eda13f90652d4ca2
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-02-21 11:59:08 +0700

    create view category

---
+++ resources/views/backend/knowledge_category/add.blade.php
@@ -0,0 +1,54 @@
+@extends('adminlte::page')
+
+@section('title', 'TFRD')
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/image_preview.css')}} ">
+@stop
+@section('content_header')
+<h1 class="pull-left">Categorires of Knowledge</h1>
+    <div class="pull-right"><a class="d-block btn btn-info" href="{{url('backend/knowledge')}}"><i class="fa fa-undo"></i> Back</a></div>
+@stop
+
+@section('content')
+<div class="pull-left col-xs-12" style="margin-top:15px;">
+    <div class="box box-warning">
+        <div class="box-header">
+            <h3 class="box-title">Add knowledge</h3>
+        </div><!-- /.box-header -->
+        <!-- form start -->
+        {{ Form::open(array('url' => 'backend/knowledge/cate/add', 'method' => 'post','enctype' => 'multipart/form-data')) }}
+        {{csrf_field()}}
+            <div class="box-body">
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input name="title" type="text" class="form-control" id="title" placeholder="Enter title of knowledge" require>
+                </div>
+                <div class="form-group">
+                    <label for="title">Description</label>
+                    <input name="description" type="text" class="form-control" id="description" placeholder="Enter title of description" require>
+                </div>
+                <div class="form-group">
+                    <label for="keywords">Keywords</label>
+                    <input name="keywords" type="text" class="form-control" id="keyword" placeholder="images,banner,tfrd,มปอ,...">
+                </div>
+                <div class="form-group">
+                    <div class="custom-preview" style="width: 300px">
+                        <label class="custom-preview-label" for="">Thumb</label>
+                        <input class="custom-preview-input" type="file" name="thumb" id="thumb" preview="thumb-preview" require>
+                    </div>
+                    <p class="help-block">Image type of png,jpg and max size is 2MB.</p>
+                    <div class="preview" id="thumb-preview"></div>
+                </div>
+              
+            </div><!-- /.box-body -->
+
+            <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+            {{ Form::close() }}
+    </div>
+</div>
+@stop
+@section('js')
+<script src="{{ asset('js/image_preview.js') }}"></script>
+@stop
