f88c746a5c2d71a6dda559a92d24ad6fd139980e
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-02-22 18:13:41 +0700

    query banner

--- resources/views/home/banner.blade.php
+++ resources/views/home/banner.blade.php
@@ -1,22 +1,23 @@
 <section class=" container-fluid">
     <div class="banner-block row h-auto">
+    @foreach($banner as $val)
         <div class="col-12 h-auto">
             <div class="container-fluid mx-auto">
-                <img class="img-fluid mx-auto" src="{{asset('images/mockup/banner.png')}}">
+                <img class="img-fluid mx-auto" src="{{asset('storage/images/banner')}}/{{$val->image}}" title="{{ $val->title }}" alt="{{ $val->seo }}">
             </div>
             <div class="container-fluid position-absolute fixed-bottom">
                 <div class="container caption">
                     <div class="col-6 offset-1">
-                        <h1>ดื่มอย่างรับผิดชอบ</h1>
+                        <h1>{{ $val->title }}</h1>
                         <div class="dash-banner"></div>
                         <p>
-                            บริโภคเครื่องดื่มแอลกอฮอล์อย่าง "รู้เท่าทัน"
-                            ปลอดภัยและรับผิดชอบต่อตน คนรอบข้าง และสังคม
+                            {{ $val->description }}
                         </p>
                     </div>
                 </div>
             </div>
         </div>
+    @endforeach
         <!--/slide-->
     </div>
     <!--/banner-block-->
