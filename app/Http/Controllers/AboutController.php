22a933a56de99876490f1ae52add9e93cacb277e
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-02-22 16:26:15 +0700

    edit controller about history

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use App\AboutMember;
+use App\AboutHistory;
 
 class AboutController extends Controller
 {
     public function index()
     {
         $member = AboutMember::all();
-        return view('about.index')->withMember($member);
+        $history = AboutHistory::find(1);
+        return view('about.index')->withMember($member)->withHistory($history);
     }
 }
