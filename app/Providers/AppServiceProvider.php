f118bf2e64411911e8aae702166b4a81d84b28d4
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-02-25 11:44:06 +0700

    query data contact set in provider (global)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\ContactData;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $contact = [];
+        try{
+            $contact =  ContactData::first();
+        } catch (Exception $e) {
+            return $e;
+        }
+        view()->share('contactDat',$contact);
     }
 
     /**
