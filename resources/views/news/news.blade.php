db08e1a6e22f59f834ad92a7668c6de82b2a92e0
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-03-13 14:47:44 +0700

    view page news

---
+++ resources/views/news/news.blade.php
@@ -0,0 +1,63 @@
+<div class="tab-pane fade show active" id="news" role="tabpanel" aria-labelledby="news-tab">
+    <div class="container">
+        <div class="row block-news">
+            <div class="col-3 mb-4">
+                <img class="img-fluid" src="{{asset('storage/images/news/activities')}}/" />
+                <p class="title mt-4">มปอ. เดินหน้าสร้าง...</p>
+                <p class="description">
+                    มูลนิธิแก้ไขปัญหาการดื่มแอลกอฮอล์ (มปอ.) เดินหน้าสานต่อกิจกรรมส่งเสริม 
+                </p>
+            </div>
+            <div class="col-3 mb-4">
+                <img class="img-fluid" src="{{asset('storage/images/news/activities')}}/" />
+                <p class="title mt-4">มปอ. เดินหน้าสร้าง...</p>
+                <p class="description">
+                    มูลนิธิแก้ไขปัญหาการดื่มแอลกอฮอล์ (มปอ.) เดินหน้าสานต่อกิจกรรมส่งเสริม 
+                </p>
+            </div>
+            <div class="col-3 mb-4">
+                <img class="img-fluid" src="{{asset('storage/images/news/activities')}}/" />
+                <p class="title mt-4">มปอ. เดินหน้าสร้าง...</p>
+                <p class="description">
+                    มูลนิธิแก้ไขปัญหาการดื่มแอลกอฮอล์ (มปอ.) เดินหน้าสานต่อกิจกรรมส่งเสริม 
+                </p>
+            </div>
+            <div class="col-3 mb-4">
+                <img class="img-fluid" src="{{asset('storage/images/news/activities')}}/" />
+                <p class="title mt-4">มปอ. เดินหน้าสร้าง...</p>
+                <p class="description">
+                    มูลนิธิแก้ไขปัญหาการดื่มแอลกอฮอล์ (มปอ.) เดินหน้าสานต่อกิจกรรมส่งเสริม 
+                </p>
+            </div>
+            <div class="col-3 mb-4">
+                <img class="img-fluid" src="{{asset('storage/images/news/activities')}}/" />
+                <p class="title mt-4">มปอ. เดินหน้าสร้าง...</p>
+                <p class="description">
+                    มูลนิธิแก้ไขปัญหาการดื่มแอลกอฮอล์ (มปอ.) เดินหน้าสานต่อกิจกรรมส่งเสริม 
+                </p>
+            </div>
+            <div class="col-3 mb-4">
+                <img class="img-fluid" src="{{asset('storage/images/news/activities')}}/" />
+                <p class="title mt-4">มปอ. เดินหน้าสร้าง...</p>
+                <p class="description">
+                    มูลนิธิแก้ไขปัญหาการดื่มแอลกอฮอล์ (มปอ.) เดินหน้าสานต่อกิจกรรมส่งเสริม 
+                </p>
+            </div>
+            <div class="col-3 mb-4">
+                <img class="img-fluid" src="{{asset('storage/images/news/activities')}}/" />
+                <p class="title mt-4">มปอ. เดินหน้าสร้าง...</p>
+                <p class="description">
+                    มูลนิธิแก้ไขปัญหาการดื่มแอลกอฮอล์ (มปอ.) เดินหน้าสานต่อกิจกรรมส่งเสริม 
+                </p>
+            </div>
+            <div class="col-3 mb-4">
+                <img class="img-fluid" src="{{asset('storage/images/news/activities')}}/" />
+                <p class="title mt-4">มปอ. เดินหน้าสร้าง...</p>
+                <p class="description">
+                    มูลนิธิแก้ไขปัญหาการดื่มแอลกอฮอล์ (มปอ.) เดินหน้าสานต่อกิจกรรมส่งเสริม 
+                </p>
+            </div>
+        </div>
+    </div>
+    @include('news.viewmore')
+</div>
---
+++ resources/views/news/viewmore.blade.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <div class="row">
+        <div class="col-2 offset-5"><a class="d-block btn-viewmore py-2">แสดงเพิ่มเติม</a></div>
+    </div>
+</div>
