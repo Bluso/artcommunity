b294f5cf858e174aaf126cd182048fa1bd1eb03f
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-03-14 15:55:47 +0700

    query content news and activities

--- resources/views/news/news.blade.php
+++ resources/views/news/news.blade.php
@@ -1,62 +1,17 @@
 <div class="tab-pane fade show active" id="news" role="tabpanel" aria-labelledby="news-tab">
     <div class="container">
         <div class="row block-news">
-            <div class="col-3 mb-4">
-                <img class="img-fluid" src="{{asset('storage/images/news/activities')}}/" />
-                <p class="title mt-4">มปอ. เดินหน้าสร้าง...</p>
-                <p class="description">
-                    มูลนิธิแก้ไขปัญหาการดื่มแอลกอฮอล์ (มปอ.) เดินหน้าสานต่อกิจกรรมส่งเสริม 
-                </p>
-            </div>
-            <div class="col-3 mb-4">
-                <img class="img-fluid" src="{{asset('storage/images/news/activities')}}/" />
-                <p class="title mt-4">มปอ. เดินหน้าสร้าง...</p>
-                <p class="description">
-                    มูลนิธิแก้ไขปัญหาการดื่มแอลกอฮอล์ (มปอ.) เดินหน้าสานต่อกิจกรรมส่งเสริม 
-                </p>
-            </div>
-            <div class="col-3 mb-4">
-                <img class="img-fluid" src="{{asset('storage/images/news/activities')}}/" />
-                <p class="title mt-4">มปอ. เดินหน้าสร้าง...</p>
-                <p class="description">
-                    มูลนิธิแก้ไขปัญหาการดื่มแอลกอฮอล์ (มปอ.) เดินหน้าสานต่อกิจกรรมส่งเสริม 
-                </p>
-            </div>
-            <div class="col-3 mb-4">
-                <img class="img-fluid" src="{{asset('storage/images/news/activities')}}/" />
-                <p class="title mt-4">มปอ. เดินหน้าสร้าง...</p>
-                <p class="description">
-                    มูลนิธิแก้ไขปัญหาการดื่มแอลกอฮอล์ (มปอ.) เดินหน้าสานต่อกิจกรรมส่งเสริม 
-                </p>
-            </div>
-            <div class="col-3 mb-4">
-                <img class="img-fluid" src="{{asset('storage/images/news/activities')}}/" />
-                <p class="title mt-4">มปอ. เดินหน้าสร้าง...</p>
-                <p class="description">
-                    มูลนิธิแก้ไขปัญหาการดื่มแอลกอฮอล์ (มปอ.) เดินหน้าสานต่อกิจกรรมส่งเสริม 
-                </p>
-            </div>
-            <div class="col-3 mb-4">
-                <img class="img-fluid" src="{{asset('storage/images/news/activities')}}/" />
-                <p class="title mt-4">มปอ. เดินหน้าสร้าง...</p>
-                <p class="description">
-                    มูลนิธิแก้ไขปัญหาการดื่มแอลกอฮอล์ (มปอ.) เดินหน้าสานต่อกิจกรรมส่งเสริม 
-                </p>
-            </div>
-            <div class="col-3 mb-4">
-                <img class="img-fluid" src="{{asset('storage/images/news/activities')}}/" />
-                <p class="title mt-4">มปอ. เดินหน้าสร้าง...</p>
-                <p class="description">
-                    มูลนิธิแก้ไขปัญหาการดื่มแอลกอฮอล์ (มปอ.) เดินหน้าสานต่อกิจกรรมส่งเสริม 
-                </p>
-            </div>
-            <div class="col-3 mb-4">
-                <img class="img-fluid" src="{{asset('storage/images/news/activities')}}/" />
-                <p class="title mt-4">มปอ. เดินหน้าสร้าง...</p>
-                <p class="description">
-                    มูลนิธิแก้ไขปัญหาการดื่มแอลกอฮอล์ (มปอ.) เดินหน้าสานต่อกิจกรรมส่งเสริม 
-                </p>
-            </div>
+            @foreach($news as $val)
+                @if($val->type == '1')
+                <div class="col-3 mb-4">
+                    <img class="img-fluid" src="{{asset('storage/images/news/thumb')}}/{{$val->thumb}}" alt="{{$val->seo}}" />
+                    <p class="title mt-4">{{$val->title}}</p>
+                    <p class="description">
+                        {{$val->description}}
+                    </p>
+                </div>
+                @endif
+            @endforeach
         </div>
     </div>
     @include('news.viewmore')
