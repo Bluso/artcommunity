bf5503ab4c2bfb52498b5f749cca07e357685105
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-03-14 15:55:21 +0700

    query news and activities

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Storage;
 use App\Banner;
+use App\NewsActivitys;
+use App\CategoriesNewsActivitys;
 
 class NewsController extends Controller
 {
     public function index()
     {
         $banner = Banner::all()->where('page_id','3');
-        return view('news.index')->withBanner($banner);
+        $cateactivity = CategoriesNewsActivitys::latest()->get();
+        $activity = NewsActivitys::latest()->get();
+        return view('news.index')->withBanner($banner)->withNews($activity)->withCate($cateactivity);
     }
 }
