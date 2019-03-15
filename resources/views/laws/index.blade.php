54a8e6c6b1b4e1cb485120a8e13a9c49a007ece6
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-03-15 17:03:42 +0700

    remove script page html

--- resources/views/laws/index.blade.php
+++ resources/views/laws/index.blade.php
@@ -12,20 +12,3 @@
     @endif
 </div>
 @stop
-
-@push('js')
-<script>
-    $('.accordion').collapse()
-    $('#accordionExample').on('hidden.bs.collapse', toggleIcon);
-    $('#accordionExample').on('shown.bs.collapse', toggleIcon);
-    
-    function toggleIcon(e) {
-        console.log('dddddddddddd');
-        $(e.target)
-            .prev('.btn-link')
-            .find(".more-less")
-            .toggleClass('fa fa-angle-down');
-    }
-    
-</script>
-@endpush
