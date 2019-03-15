6a1fe4bd7d505c2c1a14fc882a820140d57024e7
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-03-15 19:02:20 +0700

    check data page contact

--- resources/views/contact/content.blade.php
+++ resources/views/contact/content.blade.php
@@ -1,11 +1,17 @@
 <div class="container block-contact">
     <div class="row">
        <div class="col-8 offset-2">
+           @if(count($contact->name_th) != 0)
            <h2 class="text-center">{{ $contact->name_th }}</h2>
+           @endif
+           @if(count($contact->name_en) != 0)
            <h3 class="text-center">{{ $contact->name_en }}</h3>
+           @endif
+           @if(count($contact->address) != 0)
            <p class="address text-center p-4 bg-light">
            {{ $contact->address }}
            </p>
+           @endif
            <div class="row contact-data">
                 @if(count($contact->telephone) != 0)
                <div class="col text-center">
