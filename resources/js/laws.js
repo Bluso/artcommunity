0c5f1736c5bd9de05f7114a25e43f9d9f0b1e8d4
Fah Internship <morakot@praneat.com>
2019-03-28 19:46:19 +0700

    Edit toggle show laws details

--- resources/js/laws.js
+++ resources/js/laws.js
@@ -1,23 +1,24 @@
 if($('.accordion').length > 0){
-    
+
     $('.accordion').collapse();
-  
-    $('.accordion').on('hidden.bs.collapse', toggleIcon1);
-    $('.accordion').on('shown.bs.collapse', toggleIcon2);
+
+    $('.accordion').on('hidden.bs.collapse', toggleIconClose);
+    $('.accordion').on('shown.bs.collapse', toggleIconShow);
 
     $( ".nav a:first-child" ).trigger( "click" );
     $( ".tab-pane+.tab-pane").removeClass('active');
 
-    function toggleIcon1(e) {
-        console.log('hide');
+    function toggleIconClose(e) {
         $(this)
+            .find(".collapsed")
             .find(".more-less")
             .removeClass('fa-angle-down')
             .addClass('fa-angle-right');
     }
-    function toggleIcon2(e) {
-        console.log('shown');
+    function toggleIconShow(e) {
         $(this)
+            .find(".btn-link")
+            .not(".collapsed")
             .find(".more-less")
             .removeClass('fa-angle-right')
             .addClass('fa-angle-down');
