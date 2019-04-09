954799ec28988035a2e91ce3da7a9b81c65a152f
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-04-09 15:05:26 +0700

    add space bottom page knowledge

--- resources/views/knowledge/content.blade.php
+++ resources/views/knowledge/content.blade.php
@@ -2,7 +2,7 @@
  <div class="container content">
     <div class="row">
         @foreach($knowledge as $val)
-            <div class="col-3 knowledge_content">
+            <div class="col-3 knowledge_content mb-5">
                 <p class="text-indend position-absolute">{{$val->title}}</p>
                 <p class="text-indend position-absolute">{{$val->description}}</p>
                 <p class="text-indend position-absolute">{{$val->seo}}</p>
