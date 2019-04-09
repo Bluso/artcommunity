6daeb7f2101f15af65bcf8b682e2643ed4669fe0
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-04-09 14:44:54 +0700

    script category active

--- resources/js/news.js
+++ resources/js/news.js
@@ -1,8 +1,8 @@
+//load more news
 if($('.block-news').length > 0){
     var activity = $(".block-news>div");
     var countnews = activity.length;
     var newviewmore = $("#news .btn-viewmore");
-    console.log(countnews);
     if(countnews <= 9){
         newviewmore.hide();
     }
@@ -16,12 +16,11 @@ if($('.block-news').length > 0){
         }
     });
 }
-
+//load more activities
 if($('#activities .activity-block').length > 0){
     var activity = $("#activities .activity-block");
     var acviewmore = $("#activities .btn-viewmore");
     var count = activity.length;
-    console.log(count);
     if(count <= 9){
         acviewmore.hide();
     }
@@ -30,9 +29,20 @@ if($('#activities .activity-block').length > 0){
     acviewmore.on('click', function (e) {
         e.preventDefault();
         $("#activities .activity-block:hidden").slice(0, 3).slideDown();
-        console.log('$("#activities .activity-block:hidden").length',$("#activities .activity-block:hidden").length);
         if ($("#activities .activity-block:hidden").length == 0) {
             acviewmore.hide();
         }
+    });
+}
+
+//category active and show by content cate
+if($('.categories').length > 0){
+    var activity = $("#activities .activity-block");
+    $('.categories a').click( function(){
+        $('.categories a').removeClass('active');
+        $(this).addClass('active');
+        var cateId = $(this).data('cateid');
+        $(activity).hide();
+        $('.category-'+cateId).show();
     });
 }
