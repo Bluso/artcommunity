259b4e6aa812992b5df70127ddf8e58eb58c37c3
ratkamon\ratkamon-pc <ratkamon@praneat.com>
2019-04-09 18:16:03 +0700

    add section show youtube

---
+++ resources/views/knowledge/youtube.blade.php
@@ -0,0 +1,30 @@
+<section class="content-youtube mt-5 pb-5 bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="my-5 text-center">กรณีศึกษา "รู้เท่าทันแอลกอฮอล์"</h2>
+            </div>
+            <div class="col-4 mb-4" data-toggle="modal" data-target="#youtubeModal">
+                <img class="img-fluid" src="https://img.youtube.com/vi/X_GoyOCzK9Y/maxresdefault.jpg" />
+                <p class="text-center">Best Relaxing Piano Studio Ghibli Complete Collection</p>
+            </div>
+            <div class="modal fade" id="youtubeModal" tabindex="-1" role="dialog" aria-labelledby="youtubeModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="youtubeModalLabel">Best Relaxing Piano Studio Ghibli Complete Collection</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="embed-responsive embed-responsive-16by9">
+                                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/X_GoyOCzK9Y?rel=0" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
