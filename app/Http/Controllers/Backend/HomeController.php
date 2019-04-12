fd3add08ec8c679cff5f399fe33b172e3c64ccb8
Fah Internship <morakot@praneat.com>
2019-04-12 18:30:32 +0700

    insert logo to logos table

--- app/Http/Controllers/Backend/HomeController.php
+++ app/Http/Controllers/Backend/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Home;
+use App\Logo;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Storage;
@@ -17,7 +18,8 @@ class HomeController extends Controller
     public function index()
     {
         $home = Home::all();
-        return view('backend.home.index')->withHome($home);
+        $logo = Logo::all()->first();
+        return view('backend.home.index')->withHome($home)->withLogo($logo);
     }
 
     public function edit($id)
@@ -34,9 +36,9 @@ class HomeController extends Controller
             'thumb' => 'image|mimes:jpeg,png,jpg|max:2048',
         ]);
         $tbl_home = Home::find($id);
-        
-        if($request->hasfile('thumb')) 
-        { 
+
+        if($request->hasfile('thumb'))
+        {
             $file_thumb = $request->file('thumb');
             $extension_thumb = $file_thumb->getClientOriginalExtension();
             $filename_thumb =time().'.'.$extension_thumb;
@@ -48,6 +50,41 @@ class HomeController extends Controller
         $tbl_home->description = $request->description;
         $tbl_home->keywords = $request->keyword;
         $tbl_home->save();
+        return redirect('backend/home');
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title_th' => 'required|max:255',
+            'title_en' => 'required|max:255',
+            'image_title' => 'image|mimes:jpeg,png,jpg|max:2048',
+            'logo' => 'image|mimes:jpeg,png,jpg|max:2048',
+        ]);
+
+        $logo = Logo::find(1);
+        if (empty($logo)) {
+            $logo = new Logo;
+        }
+        $logo->title_th = $request->title_th;
+        $logo->title_en = $request->title_en;
+        if($request->hasfile('logo'))
+        {
+            $file_logo = $request->file('logo');
+            $extension = $file_logo->getClientOriginalExtension();
+            $filename_logo = time().'.'.$extension;
+            Storage::disk('public')->putFileAs('images/home/logo',$file_logo, $filename_logo);
+            $logo->logo = $filename_logo;
+        }
+        if($request->hasfile('image_title'))
+        {
+            $file_image_title = $request->file('image_title');
+            $extension = $file_image_title->getClientOriginalExtension();
+            $filename_image_title = time().'.'.$extension;
+            Storage::disk('public')->putFileAs('images/home/image_title',$file_image_title, $filename_image_title);
+            $logo->img_title_th = $filename_image_title;
+        }
+        $logo->save();
         return redirect('backend/home');
     }
 }
