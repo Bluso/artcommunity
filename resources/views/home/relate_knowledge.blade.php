b2bdcaeda19d6a83844d599803f728a66132ad03
Fah Internship <morakot@praneat.com>
2019-04-19 16:14:03 +0700

    font size and align center of knowledge - mobile size

--- resources/views/home/relate_knowledge.blade.php
+++ resources/views/home/relate_knowledge.blade.php
@@ -2,13 +2,13 @@
 <section class="container relate_knowledge">
     <div class="row">
         <div class="col-12">
-            <h2 class="text-center">ความรู้และงานวิจัย</h2>
+            <h2 class="text-center h2-font">ความรู้และงานวิจัย</h2>
             <div class="dash"></div>
         </div>
     </div>
     <div class="row">
         @foreach($knowledge as $val)
-            <div class="col-12 col-sm-6 col-lg-3 mb-3 knowledge_content">
+            <div class="col-12 col-sm-6 col-lg-3 mb-3 knowledge_content align-center">
                 <p class="text-indend position-absolute">{{$val->title}}</p>
                 <p class="text-indend position-absolute">{{$val->description}}</p>
                 <p class="text-indend position-absolute">{{$val->seo}}</p>
