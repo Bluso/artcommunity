f153d024dfdaaf4e558ee3c2574e83e94d995cc0
DESKTOP-OQRE45A\Praneat-Art <prapot@praneat.com>
2019-04-19 16:43:24 +0700

    detail content center

--- resources/views/news_detail/content.blade.php
+++ resources/views/news_detail/content.blade.php
@@ -1,5 +1,5 @@
 <div class="row mt-5 content-detail">
-    <div class="col-10 offset-2">
+    <div class="col-lg-10 offset-lg-2">
         <div class="row">
             <div class="col-12 text-center">
                 <img class="mx-auto img-fluid" src="{{asset('storage/images/news')}}/{{$val->image}}" alt="{{$val->seo}}"/>
