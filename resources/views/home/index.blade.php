4fe109a921ee3eae51bf0756debb7775af558bb7
Fah Internship <morakot@praneat.com>
2019-04-19 16:11:39 +0700

    align center of activities in home page - mobile size

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -11,7 +11,7 @@
             <div class="col-sm-6 col-lg-4">
                 <a class="row" href="{{ $h->url }}">
                     <p class="text-indend">{{ $h->title.' '.$h->keyword }}</p>
-                    <div class="col-12">
+                    <div class="col-12 align-center">
                         <img class="img-fluid" src="{{asset('storage/images/home/thumb')}}/{{$h->thumb}}" alt="{{ $h->title.' '.$h->keyword }}">
                     </div>
                     <article class="col-12 mt-4">
