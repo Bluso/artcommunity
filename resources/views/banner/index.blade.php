268d5f14704059e36eb293cf05ca434e7b67e354
Fah Internship <morakot@praneat.com>
2019-04-19 16:12:50 +0700

    font banner except home page - mobile size

--- resources/views/banner/index.blade.php
+++ resources/views/banner/index.blade.php
@@ -4,7 +4,7 @@
     @foreach($banner as $val)
         <div class="col-12 h-auto position-relative p-0 m-0">
             <p class="text-indend position-absolute">{{ $val->title.' '.$val->seo }}</p>
-            <h2 class="position-absolute text-center">{{ $val->title }}</h2>
+            <h2 class="position-absolute text-center banner-font">{{ $val->title }}</h2>
             <div class="container-fluid mx-auto p-0 m-0">
               <div class="dark-overlay">
                 <img class="img-fluid mx-auto p-0 m-0 w-100" src="{{asset('storage/images/banner')}}/{{$val->image}}" title="{{ $val->title }}" alt="{{ $val->seo }}">
