17e8882401514d673c5c337586b1ef5edc100f56
DESKTOP-OQRE45A\Praneat-Art <prapot@praneat.com>
2019-04-19 16:45:49 +0700

    news detail contant center relate

--- resources/views/news_detail/relate.blade.php
+++ resources/views/news_detail/relate.blade.php
@@ -10,12 +10,12 @@
         <div class="row">
             <div class="col-12 mt-4">
                 <div class="row news-slide">
-                    @foreach($activities as $val)
-                    <a href="{{url('news/detail')}}/{{$val->id}}" class="col-4 activity-block">
+                    @foreach($activities as $key => $val)
+                    <a href="{{url('news/detail')}}/{{$val->id}}" class="col-12 col-sm-4 activity-block">
                         <p class="text-indend position-absolute">{{$val->title}}</p>
                         <p class="text-indend position-absolute">{{$val->description}}</p>
                         <p class="text-indend position-absolute">{{$val->seo}}</p>
-                        <div><img class="img-fluid w-100" src="{{asset('storage/images/news/thumb')}}/{{$val->thumb}}" alt="{{$val->seo}}"></div>
+                        <div class="@if($key != 0) mb-xs-3 @endif"><img class="img-fluid w-100" src="{{asset('storage/images/news/thumb')}}/{{$val->thumb}}" alt="{{$val->seo}}"></div>
                         <article class="pl-3 pb-3">
                             <p>โครงการ</p>
                             <div></div>
