733dc721326294a139796d701b464a8a8f1c0c49
DESKTOP-OQRE45A\Praneat-Art <prapot@praneat.com>
2019-04-19 16:43:46 +0700

    detail contant footer

--- resources/views/news_detail/footer.blade.php
+++ resources/views/news_detail/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-10 offset-2">
+    <div class="col-lg-10 offset-lg-2">
         <hr>
         @if($val->file)
         <a href="{{asset('storage/files/news')}}/{{$val->file}}" class="col-4 float-right btn btn-outline-warning mt-3 py-2">
