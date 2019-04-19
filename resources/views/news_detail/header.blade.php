ea289892db077faf20b405c27620b01e6c36b98d
DESKTOP-OQRE45A\Praneat-Art <prapot@praneat.com>
2019-04-19 16:45:19 +0700

    detail contant header

--- resources/views/news_detail/header.blade.php
+++ resources/views/news_detail/header.blade.php
@@ -1,8 +1,8 @@
 <div class="row header mt-5">
-    <div class="col-10 offset-2">
+    <div class="col-lg-10 offset-lg-2">
         <div class="row">
             <div class="col-3">
-                <img class="img-fluid" src="{{asset('storage/images/cate_news')}}/{{$val->cate->thumb}}" alt="{{$val->cate->seo}}"/> 
+                <img class="img-fluid" src="{{asset('storage/images/cate_news')}}/{{$val->cate->thumb}}" alt="{{$val->cate->seo}}"/>
             </div>
             <div class="col-9">
                 <h2>{{$val->title}}</h2>
