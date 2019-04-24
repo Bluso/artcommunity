8288c32dd88f565fc79dc743cd9f735181ff4b98
Fah Internship <morakot@praneat.com>
2019-04-24 18:36:59 +0700

    hambergur menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,4 +1,39 @@
-<nav class="container">
+<nav class="navbar navbar-expand-lg navbar-light d-md-none">
+  <img class="img-fluid" src="{{asset('storage/images/home/logo')}}/{{$logo->logo}}" alt="{{$logo->title_th}} {{$logo->title_en}}"/>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+    @if(!empty($logo->img_title_th))
+        <h2 class="my-1 w-100"><img class="img-fluid" src="{{asset('storage/images/home/image_title')}}/{{$logo->img_title_th}}" alt="{{$logo->title_th}} {{$logo->title_en}}"/></h2>
+    @else
+        <h3 class="title_th w-100">{{$logo->title_th}}</h3>
+    @endif
+    <h3 class="w-100">{{$logo->title_en}}</h3>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul id="menu-bar" class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link p-home" href="{{ url('/') }}">หน้าแรก</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link p-about" href="{{ url('about') }}">เกี่ยวกับเรา</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link p-news" href="{{ url('news') }}">ข่าวสารและกิจกรรม</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link p-laws" href="{{ url('laws') }}">กฏหมายที่เกี่ยวข้อง</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link p-knowledge" href="{{ url('knowledge') }}">บทความงานวิจัย</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link p-contact" href="{{ url('contact') }}">ติดต่อเรา</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<nav class="container d-none d-md-block">
     <div class="row my-2">
         <div class="col-12">
             <h1 class="text-indend">{{$logo->title_th}} {{$logo->title_en}}</h1>
@@ -14,17 +49,17 @@
         </div>
     </div>
 </nav>
-<nav class="py-3 bg-grey">
+<nav class="py-3 bg-grey d-none d-md-block">
     <div class="container">
         <div class="row">
             <div class="col-11">
-                <ul class="list-unstyled w-100">
-                    <li class="mr-4 float-left"><a href="{{ url('/') }}">หน้าแรก</a></li>
-                    <li class="mr-4 float-left"><a href="{{ url('about') }}">เกี่ยวกับเรา</a></li>
-                    <li class="mr-4 float-left"><a href="{{ url('news') }}">กิจกรรมและข่าวสาร</a></li>
-                    <li class="mr-4 float-left"><a href="{{ url('laws') }}">กฏหมายที่เกี่ยวข้อง</a></li>
-                    <li class="mr-4 float-left"><a href="{{ url('knowledge') }}">บทความงานวิจัย</a></li>
-                    <li class="float-left"><a href="{{ url('contact') }}">ติดต่อเรา</a></li>
+                <ul id="menu-bar" class="list-unstyled w-100">
+                    <li class="mr-4 float-left"><a class="p-home" href="{{ url('/') }}">หน้าแรก</a></li>
+                    <li class="mr-4 float-left"><a class="p-about" href="{{ url('about') }}">เกี่ยวกับเรา</a></li>
+                    <li class="mr-4 float-left"><a class="p-news" href="{{ url('news') }}">ข่าวสารและกิจกรรม</a></li>
+                    <li class="mr-4 float-left"><a class="p-laws" href="{{ url('laws') }}">กฏหมายที่เกี่ยวข้อง</a></li>
+                    <li class="mr-4 float-left"><a class="p-knowledge" href="{{ url('knowledge') }}">บทความงานวิจัย</a></li>
+                    <li class="float-left"><a class="p-contact" href="{{ url('contact') }}">ติดต่อเรา</a></li>
                 </ul>
             </div>
             <div class="col-1 d-none">
