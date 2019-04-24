b566ddaa46f9153b8391a631f64cbfef825f4240
Fah Internship <morakot@praneat.com>
2019-04-24 12:17:46 +0700

    img content news

--- resources/views/news_detail/content.blade.php
+++ resources/views/news_detail/content.blade.php
@@ -2,7 +2,7 @@
     <div class="col-lg-10 offset-lg-2">
         <div class="row">
             <div class="col-12 text-center">
-                <img class="mx-auto img-fluid" src="{{asset('storage/images/news')}}/{{$val->image}}" alt="{{$val->seo}}"/>
+                <img class="mx-auto" src="{{asset('storage/images/news')}}/{{$val->image}}" alt="{{$val->seo}}"/>
             </div>
         </div>
         <div class="row mt-5">
